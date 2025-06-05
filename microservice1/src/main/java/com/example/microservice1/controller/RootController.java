eab14ecf4fbe68ce385d6afd931bc69be6ce7304
Dung Pham <leonardcurryst@gmail.com>
2025-06-05 16:23:09 +0000

    update prefix

---
+++ microservice1/src/main/java/com/example/microservice1/controller/RootController.java
@@ -0,0 +1,13 @@
+package com.example.microservice1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to Microservice 1!";
+    }
+}
