edb6f8773b62ed796a7287f317f4e910ca2639b1
Dung Pham <leonardcurryst@gmail.com>
2025-06-05 17:03:47 +0000

    add getter and setter

--- microservice1/src/main/java/com/example/microservice1/model/Customer.java
+++ microservice1/src/main/java/com/example/microservice1/model/Customer.java
@@ -10,6 +10,41 @@ public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
-    // getter/setter...
+    private String email;
+
+    // Constructors
+    public Customer() {}
+
+    public Customer(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    // Getters
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    // Setters
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
