6df6260015062d6fe4f27c1ce41435b87825991b
Dung Pham <leonardcurryst@gmail.com>
2025-06-06 17:34:38 +0000

    update be2

--- microservice2/src/main/java/com/example/microservice2/model/Order.java
+++ microservice2/src/main/java/com/example/microservice2/model/Order.java
@@ -3,10 +3,12 @@ package com.example.microservice2.model;
 import jakarta.persistence.*;
 
 @Entity
+@Table(name = "orders")
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String description;
     private Double amount;
 
