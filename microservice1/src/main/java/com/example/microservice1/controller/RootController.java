1532436dd08306d85e10f47a8bdc0e431e884247
Dung Pham <leonardcurryst@gmail.com>
2025-06-07 05:01:39 +0000

    update root request mapping

--- microservice1/src/main/java/com/example/microservice1/controller/RootController.java
+++ microservice1/src/main/java/com/example/microservice1/controller/RootController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RootController {
 
-    @GetMapping("/api")
+    @GetMapping("/")
     public String home() {
         return "Welcome to Microservice 1!";
     }
